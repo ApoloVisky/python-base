4f28109b63accfecfb99fdbf0ef2367631d534c3
ApoloVisky <paraminhaalegria000@gmail.com>
2023-10-02 17:33:59 -0300

    final de projeto base

--- main.py
+++ main.py
@@ -1,1 +1,36 @@
-print("Hello World")
+#!/usr/bin/env python3
+"""Hello World multi linguas.
+
+Dependendo da lingua configurada o programa exibe a correspondente
+
+Como usar:
+
+Tenha a variável LANG devidamente configurada no seu sistema operacional:
+
+$ export LANG=pt_BR
+
+execução:
+
+$ python3 main.py
+ou
+$ ./main.py
+"""
+import os
+__version__ = '0.1'
+__author__ = 'Adeilton da Silva'
+__license__ = 'Unlicense'
+
+current_language = os.getenv('LANG', 'en_US')[:5]
+
+msg =("Hello world")
+
+if current_language=="pt_BR":
+    msg=("Olá Mundo")
+
+
+elif current_language=="it_IT":
+     msg=("Ciao Mondo")
+
+
+
+print(msg) 
